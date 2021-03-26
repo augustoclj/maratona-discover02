dafe8917ef3b70d1a0549e52731c0edcb8c5a905
Augusto Julietti <augusto.julietti@gmail.com>
2021-03-25 23:10:15 -0300

    Aula 1

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const server = express()
+const routes = require("./routes")
+//usando template engine
+server.set('view engine', 'ejs')
+
+//habilitar arquivos estaticos
+server.use(express.static("public"));
+
+server.use(routes);
+
+server.listen(3000, () => console.log('rodando'))
